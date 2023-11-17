675aa15c2520feefd6b4eccd8918bba5f91fef4b
killermax <thekillermax1999@gmail.com>
2023-11-17 11:49:42 +0530

    api configured

---
+++ src/middleware.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+
+async function apicall(q) {
+  const { data } = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${q}&appid=${process.env.API_KEY}&units=metric`
+  );
+  const { list, city } = data;
+  const {name,country, sunrise, sunset} = city
+  let newlist = []
+  for (let i = 1; i < list.length; i += 8) {
+    newlist.push(list[i]);
+  }
+  // newlist.push(list[39])
+
+  return {
+    list: newlist,
+    location: name + ", " + country,
+    sunrise: convertUnixTimestampToIndianTime(sunrise),
+    sunset: convertUnixTimestampToIndianTime(sunset)
+  }
+  // return result.data
+}
+
+// Function to convert Unix timestamp to human-readable date
+function convertUnixTimestampToIndianTime(unixTimestamp) {
+  // Create a new Date object with the provided Unix timestamp in milliseconds
+  const date = new Date(unixTimestamp * 1000);
+
+  const istDate = new Date(date.getTime());
+
+  // Format the date components to a readable string
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: 'Asia/Kolkata', // Set timezone to Indian Standard Time
+    timeZoneName: 'short',
+  };
+
+  const formattedDate = istDate.toLocaleString('en-IN', options);
+  return formattedDate;
+}
+
+// Example Unix timestamp (replace this with your Unix timestamp)
+
+
+
+module.exports = {
+  apicall,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,15 @@
 const express = require("express");
-const axios = require("axios");
 const router = express.Router();
+const { apicall } = require("./middleware");
 
 router.get("/", function (req, res) {
   res.json({ status: "success", message: "Welcome to Weather API" });
 });
 
-router.post("/getWeather", async (req, res) => {
-  const { value } = await req.body;
-  try {
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${process.env.API_KEY}&units=metric`
-    );
-    const { name, sys, main, weather, cod } = await data;
-
-    if (cod == 200)
-      res.json({
-        cod: cod,
-        city: name,
-        ISO: sys.country,
-        weather: weather[0],
-        main: main
-      });
-    else res.json({ cod: 404, message: "city not found" });
-
-    // res.json(response);
-  } catch {
-    res.json({
-      cod: "404",
-      message: "city not found",
-    });
-  }
+router.get("/getWeather/:city", async (req, res) => {
+  const data = req.params.city;
+  const result = await apicall(data)
+  res.json(result)
 });
-
-
 
 module.exports = router;
